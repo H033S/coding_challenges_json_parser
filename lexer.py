c752119ab18ad5a9365e35209a04bef70a0fc38c
Antonio Nazco <tonito.nazco@gmail.com>
2024-07-24 22:11:41 -0400

    Modified based on writing-a-simple-json-parser ART

--- lexer.py
+++ lexer.py
@@ -1,5 +1,4 @@
 from enum import Enum
-from functools import partial
 
 
 class TokenType(Enum):
@@ -8,18 +7,20 @@ class TokenType(Enum):
     OPEN_SQUARE_BRACKET = 3
     CLOSED_SQUARE_BRACKET = 4
     SEMI_COLON = 5
+    DOUBLE_QUOTE = 11
     NUMBER = 6
     BOOL = 7
     TEXT = 8
     SKIP = 9
-    DOUBLE_QUOTE = 10
 
 
 class Token:
 
-    def __init__(self, type: TokenType, value: str) -> None:
-        self.type: TokenType = type
-        self.value: str = value
+    def __init__(self, token_type: TokenType, value) -> None:
+        assert type(value) in [int, float, str, bool], "Error value {}".format(value)
+
+        self.type: TokenType = token_type
+        self.value = value
 
     def __str__(self) -> str:
         return f"Token<{self.value} : {self.type}>"
@@ -28,60 +29,144 @@ class Token:
         return f"Token<{self.value} : {self.type}>"
 
 
-def getTokenForMultiChar(multi_char_value: str) -> Token:
-    if multi_char_value in ["true", "false"]:
-        return Token(TokenType.BOOL, multi_char_value)
-    elif multi_char_value.replace(".", "").isdecimal():
-        return Token(TokenType.NUMBER, multi_char_value)
-    else:
-        return Token(TokenType.TEXT, multi_char_value)
-
-
 def tokenize(source: str) -> list[Token]:
 
     tokens: list[Token] = []
     addToken = tokens.append
-    multi_char_value: str = ""
 
-    def addMultiCharTokenIfExist():
-        nonlocal multi_char_value
-        if multi_char_value:
-            addToken(getTokenForMultiChar(multi_char_value))
-            multi_char_value = ""
+    while len(source):
 
-    for current_char in source:
-        newToken = partial(Token, value=current_char)
+        print(source)
 
-        if current_char == "{":
-            addMultiCharTokenIfExist()
-            addToken(newToken(TokenType.OPEN_CURLY_BRACKET))
-        elif current_char == "}":
-            addMultiCharTokenIfExist()
-            addToken(newToken(TokenType.CLOSED_CURLY_BRACKET))
-        elif current_char == "[":
-            addMultiCharTokenIfExist()
-            addToken(newToken(TokenType.OPEN_SQUARE_BRACKET))
-        elif current_char == "]":
-            addMultiCharTokenIfExist()
-            addToken(newToken(TokenType.CLOSED_SQUARE_BRACKET))
-        elif current_char == ":":
-            addMultiCharTokenIfExist()
-            addToken(newToken(TokenType.SEMI_COLON))
-        elif current_char == '"':
-            addMultiCharTokenIfExist()
-            addToken(newToken(TokenType.DOUBLE_QUOTE))
-        elif current_char in [" ", "\t", "\n"]:
-            addMultiCharTokenIfExist()
-            addToken(newToken(TokenType.SKIP))
-        else:
-            if current_char.isalpha():
-                multi_char_value += current_char
-            elif current_char.isdecimal() or current_char == ".":
-                multi_char_value += current_char
-            else:
-                raise Exception("Not Supported Character")
+        result, source = check_source_for_str(source)
+        if result is not None:
+            addToken(result)
             continue
-    else:
-        addMultiCharTokenIfExist()
+
+        result, source = check_source_for_number(source)
+        if result is not None:
+            addToken(result)
+            continue
+
+        result, source = check_source_for_bool(source)
+        if result is not None:
+            addToken(result)
+            continue
+
+        result, source = check_source_for_skip(source)
+        if result is not None:
+            addToken(result)
+            continue
+
+        result, source = check_source_for_brackets(source)
+        if result is not None:
+            addToken(result)
+            continue
+
+        raise Exception("Unexpected Character {}".format(source[0]))
 
     return tokens
+
+
+def check_source_for_str(source: str):
+
+    DOUBLE_QUOTE = '"'
+
+    if source[0] != DOUBLE_QUOTE:
+        return None, source
+
+    source = source[1:]
+
+    result = ""
+    for c in source:
+        if c == DOUBLE_QUOTE:
+            result_offset = len(result) + 1
+            return Token(TokenType.TEXT, result), source[result_offset:]
+        else:
+            result += c
+
+    raise Exception("Not closing quotes")
+
+
+def check_source_for_number(source: str):
+
+    NUMBER_CHAR = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-", "e", "."]
+
+    if not source[0].isdigit():
+        return None, source
+
+    result = ""
+    for c in source:
+        if c in NUMBER_CHAR:
+            result += c
+        else:
+            break
+
+    try:
+        result_offset = len(result)
+        source = source[result_offset:]
+
+        if "." in result:
+            result = Token(TokenType.NUMBER, float(result))
+        else:
+            result = Token(TokenType.NUMBER, int(result))
+
+        return result, source
+
+    except Exception as e:
+        raise Exception("Incorrect number format") from e
+
+
+def check_source_for_bool(source: str):
+
+    result = None
+
+    if source.startswith("true"):
+        result_offset = len("true")
+        source = source[result_offset:]
+        result = Token(TokenType.BOOL, True)
+
+    elif source.startswith("false"):
+        result_offset = len("false")
+        source = source[result_offset:]
+        result = Token(TokenType.BOOL, False)
+
+    return result, source
+
+
+def check_source_for_skip(source: str):
+
+    SKIP_VALUE = (" ", "\t", "\n")
+    result = None
+
+    if source.startswith(SKIP_VALUE):
+        result_offset = 1
+        result = Token(TokenType.SKIP, source[0])
+        source = source[result_offset:]
+
+    return result, source
+
+
+def check_source_for_brackets(source: str):
+
+    SEPARATORS = ("{", "}", "[", "]", ":")
+    result = None
+
+    if source.startswith("{"):
+        result = Token(TokenType.OPEN_CURLY_BRACKET, source[0])
+    elif source.startswith("}"):
+        result = Token(TokenType.CLOSED_CURLY_BRACKET, source[0])
+
+    elif source.startswith("["):
+        result = Token(TokenType.OPEN_SQUARE_BRACKET, source[0])
+    elif source.startswith("]"):
+        result = Token(TokenType.CLOSED_SQUARE_BRACKET, source[0])
+
+    elif source.startswith(":"):
+        result = Token(TokenType.SEMI_COLON, source[0])
+
+    if source.startswith(SEPARATORS):
+        result_offset = 1
+        source = source[result_offset:]
+
+    return result, source
--- test_lexer.py
+++ test_lexer.py
@@ -18,7 +18,7 @@ def test_checking_punctuation_tokens():
 
 
 def test_checking_word_tokenization():
-    source = "word"
+    source = '"word"'
     tokens = lexer.tokenize(source)
 
     assert len(tokens) == 1, "Invalid Size on Tokens"
@@ -32,7 +32,7 @@ def test_checking_number_tokenization():
 
     assert len(tokens) == 1, "Invalid Size on Tokens"
     assert tokens[0].type == lexer.TokenType.NUMBER, "Invalid Type for Token"
-    assert tokens[0].value == "2.5"
+    assert tokens[0].value == 2.5
 
 
 def test_checking_bool_tokenization():
@@ -41,24 +41,22 @@ def test_checking_bool_tokenization():
 
     assert len(tokens) == 3, "Invalid Size on Tokens"
     assert tokens[0].type == lexer.TokenType.BOOL, "Invalid Type for Token"
-    assert tokens[0].value == "true"
+    assert tokens[0].value == True
     assert tokens[1].type == lexer.TokenType.SKIP, "Invalid Type for Token"
     assert tokens[1].value == " "
     assert tokens[2].type == lexer.TokenType.BOOL, "Invalid Type for Token"
-    assert tokens[2].value == "false"
+    assert tokens[2].value == False
 
 
 def test_simple_object():
     source = '{\n"a":3.5\n}'
     tokens = lexer.tokenize(source)
 
-    assert len(tokens) == 9
+    assert len(tokens) == 7
     assert tokens[0].type == lexer.TokenType.OPEN_CURLY_BRACKET
     assert tokens[1].type == lexer.TokenType.SKIP
-    assert tokens[2].type == lexer.TokenType.DOUBLE_QUOTE
-    assert tokens[3].type == lexer.TokenType.TEXT
-    assert tokens[4].type == lexer.TokenType.DOUBLE_QUOTE
-    assert tokens[5].type == lexer.TokenType.SEMI_COLON
-    assert tokens[6].type == lexer.TokenType.NUMBER
-    assert tokens[7].type == lexer.TokenType.SKIP
-    assert tokens[8].type == lexer.TokenType.CLOSED_CURLY_BRACKET
+    assert tokens[2].type == lexer.TokenType.TEXT
+    assert tokens[3].type == lexer.TokenType.SEMI_COLON
+    assert tokens[4].type == lexer.TokenType.NUMBER
+    assert tokens[5].type == lexer.TokenType.SKIP
+    assert tokens[6].type == lexer.TokenType.CLOSED_CURLY_BRACKET
